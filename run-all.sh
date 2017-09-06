29290d96b306d84da21a6fc270af5cc03a987f3c
Kevin Dice <kevin.dice1@gmail.com>
2017-09-06 03:16:36 -0500

    Clean up run scripts and randomize order

---
+++ run-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -t 1:20
+uname -a
+
+for i in `python -c "import random; y=range(0,27); print(' '.join(list(map(lambda x: str(2 ** x), random.sample(y, len(y))))))"`
+do
+    for v in `python -c "import random; versions=['block2', 'block4', 'block8', 'compiler_optimized', 'default', 'o_fast', 'unroll16', 'unroll8']; print(' '.join(random.sample(versions, len(versions))))"`
+    do
+
+    /homes/kmdice/625-hw2/pt0_$v $i
+
+    done
+done
--- sub-default.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#$ -t 1:20
-uname -a
-
-for i in `python -c "import random; y=range(0,27); print(' '.join(list(map(lambda x: str(2 ** x), random.sample(y, len(y))))))"`
-do
-    /homes/kmdice/625-hw2/pt0_default $i
-done
