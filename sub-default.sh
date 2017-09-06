90d44f8991e0ff4fdaec657a5fe8ec5b71a71d27
Kevin Dice <kevin.dice1@gmail.com>
2017-09-05 22:37:39 -0500

    Fix executable path

--- sub-default.sh
+++ sub-default.sh
@@ -3,5 +3,5 @@ uname -a
 
 for i in `python -c "import random; y=range(0,27); print(' '.join(list(map(lambda x: str(2 ** x), random.sample(y, len(y))))))"`
 do
-    ~/625/pt0 $i
+    /homes/kmdice/625-hw2/pt0 $i
 done
